9d1b2e6fa1cb2237cece6891787a9bafa9fb0035
Roger Fernandes <roger.afernandes@hotmail.com>
2021-06-29 09:17:12 -0300

    refactor: I changed userIcon to userIcon Link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
-import { FiSearch, FiAlertTriangle, FiUser, FiMenu } from 'react-icons/fi';
+import { FiUser } from 'react-icons/fi';
 
+import Link from 'next/link';
+
+import { useAuth } from '../../hooks/auth';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { user } = useAuth();
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerWrapper}>
@@ -11,18 +16,12 @@ export function Header() {
           <h1>Leilões</h1>
         </div>
         <div className={styles.buttons}>
-          <button className={styles.button}>
-            <FiSearch />
-          </button>
-          <button className={styles.button}>
-            <FiAlertTriangle />
-          </button>
-          <button className={styles.button}>
-            <FiUser />
-          </button>
-          <button className={styles.button}>
-            <FiMenu />
-          </button>
+          <Link href="/login">
+            <button className={styles.button}>
+              <FiUser />
+              <span>{user?.name}</span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
