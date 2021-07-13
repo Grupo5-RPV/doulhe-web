184cb088e14107151ab037cb40fb45c3b03604a3
Thalis Antunes <thalisantunes@hotmail.com>
2021-07-13 16:47:21 -0300

    feat:function add paritcipant

--- src/pages/participant/index.tsx
+++ src/pages/participant/index.tsx
@@ -1,70 +1,104 @@
+import { phone } from 'faker/locale/zh_TW';
 import React, { useState } from 'react';
-import {Header} from '../../components/Header';
+import { Header } from '../../components/Header';
 import styles from './participant.module.scss';
 
 
-export default function Item(){
-
-const [nome, setNome] = useState('');
-const [email, setEmail] = useState('');
-const [senha, setSenha] = useState('');
-const [user, setUser] = useState('');
-const [endereco, setEndereco] = useState('');
-const [telefone, setTelefone] = useState('');
-
-async function click(event){
-  event.preventDefault();
+interface Data {
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+  adress: string;
+  phone: string;
 }
 
-const data = {
-  "name": nome,
-  "username": user,
-  "password": senha,
-  "email": email,
-  "adress": endereco,
-  "phone": telefone,
-}
+export default function Participant() {
+  
+  const {addParticipant, handleSubmit} = useForm();
+  const {save} = useAdd();
+  
+  async function handleSave (data: Data){
+    await save({ 
+      name: data.name,
+      username: data.username,
+      password: data.password,
+      email: data.email,
+      adress: data.adress,
+      phone: data.phone,
+    });
+  }
+  
+  
+  
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [user, setUser] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [telefone, setTelefone] = useState('');
 
+  async function click(event) {
+    event.preventDefault();
+  }
 
-try {
- // const response = await api.post("participant", data)
+  const data = {
+    name: nome,
+    username: user,
+    password: senha,
+    email: email,
+    adress: endereco,
+    phone: telefone,
+  };
 
-}catch (ex) {
-  alert(ex.response.data.message)
-}
+  return (
+    <div className={styles.containner}>
+      <Header />
 
-
-    return(
-        <div className={styles.containner}>
-            <Header/>
-       
-
-        <div className={styles.sectionForm}>
+      <div className={styles.sectionForm}>
         <h2>Preencha seus dados</h2>
         <form>
-        <input type="text" placeholder="Nome"   onChange = {event => setNome(event.target.value)}/>
-        <input type="email" placeholder="E-mail" onChange = {event => setEmail(event.target.value)}/>
-        <input type="password" placeholder="Senha" onChange = {event => setSenha(event.target.value)}/>
-        <input type="password" placeholder="Confirmar senha" onChange = {event => setSenha(event.target.value)}/>
-        <input type="text" placeholder="User" onChange = {event => setUser(event.target.value)}/>
-        <input type="text" placeholder="Endereço" onChange = {event => setEndereco(event.target.value)}/>
-        <input type="phone" placeholder="Telefone" onChange = {event => setTelefone(event.target.value)}/>
-       
-        <button type="submit"  onClick={click}>Salvar</button>
+          <input
+            type="text"
+            placeholder="Nome"
+            onChange={(event) => setNome(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(event) => setSenha(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirmar senha"
+            onChange={(event) => setSenha(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="User"
+            onChange={(event) => setUser(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Endereço"
+            onChange={(event) => setEndereco(event.target.value)}
+          />
+          <input
+            type="phone"
+            placeholder="Telefone"
+            onChange={(event) => setTelefone(event.target.value)}
+          />
+
+          <button type="submit" onClick={click}>
+            Salvar
+          </button>
         </form>
-        </div>
-
-
-        </div>
-
-
-
-
-
-
-
-
-
-
-    );
+      </div>
+    </div>
+  );
 }
