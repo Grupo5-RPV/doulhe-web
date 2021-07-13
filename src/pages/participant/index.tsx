a57044faa0d1b3c6a2ab6e8413821575a8fb9b8f
Thalis Antunes <thalisantunes@hotmail.com>
2021-07-13 16:04:44 -0300

    feat: function add participant

--- src/pages/participant/index.tsx
+++ src/pages/participant/index.tsx
@@ -1,7 +1,39 @@
+import React, { useState } from 'react';
 import {Header} from '../../components/Header';
 import styles from './participant.module.scss';
 
+
 export default function Item(){
+
+const [nome, setNome] = useState('');
+const [email, setEmail] = useState('');
+const [senha, setSenha] = useState('');
+const [user, setUser] = useState('');
+const [endereco, setEndereco] = useState('');
+const [telefone, setTelefone] = useState('');
+
+async function click(event){
+  event.preventDefault();
+}
+
+const data = {
+  "name": nome,
+  "username": user,
+  "password": senha,
+  "email": email,
+  "adress": endereco,
+  "phone": telefone,
+}
+
+
+try {
+ // const response = await api.post("participant", data)
+
+}catch (ex) {
+  alert(ex.response.data.message)
+}
+
+
     return(
         <div className={styles.containner}>
             <Header/>
